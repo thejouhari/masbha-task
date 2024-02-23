55c6d2a37dfaf0347cf6225198aac20871e255fe
Jouhari <jouhari>
2024-02-23 14:46:58 +0200

    themasbha

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
 const counter = document.querySelectorAll(".counter");
 const btn = document.querySelectorAll("button");
 
-let count1=0;
-let count2=0;
-let count3=0;
-let count4=0;
+for(let i=0; i<btn.length; i++){
 
-btn[0].onclick = function(){
-    count1++;
-    counter[0].innerHTML = count1;
-}
-
-btn[1].onclick = function(){
-    count2++;
-    counter[1].innerHTML = count2;
-}
-
-btn[2].onclick = function(){
-    count3++;
-    counter[2].innerHTML = count3;
-}
-
-btn[3].onclick = function(){
-    count4++;
-    counter[3].innerHTML = count4;
+    let count=0;
+    btn[i].onclick = function(){
+        count++;
+        counter[i].innerHTML = count;
+    }
+    
 }
 
 
